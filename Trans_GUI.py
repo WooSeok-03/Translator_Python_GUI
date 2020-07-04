90c3ca2f1023b1e69ab371f9bf7ef16264510267
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2020-07-04 13:07:50 +0900

    Add_Translation_Function
    ComboBox에서 영어 & 한국어 선택기능 추가
    Original_Box의 내용을 번역하여 Trans_Box에 보여주는 기능 추가

--- Trans_GUI.py
+++ Trans_GUI.py
@@ -3,6 +3,7 @@ from googletrans import Translator
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
+#번역을 위한 객체 생성
 translator = Translator()
 
 #UI파일 연결(ui파일이 python 코드 파일과 같은 디렉토리에 있어야함.)
@@ -13,6 +14,27 @@ class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        self.btn_Translation.clicked.connect(self.Trans_)
+
+    def Trans_(self):
+        #comboBox 선택
+        if self.Original_language.currentIndex() == 0:
+            lang_src = 'en'
+        elif self.Original_language.currentIndex() == 1:
+            lang_src = 'ko'
+
+        if self.Trans_language.currentIndex() == 0:
+            lang_dest = 'ko'
+        elif self.Trans_language.currentIndex() == 1:
+            lang_dest = 'en'
+
+        #번역기능
+        original = self.Original_Box.toPlainText()
+        trans_show = translator.translate(original, src=lang_src, dest=lang_dest).text
+
+        self.Trans_Box.setText(trans_show)
+
 
 
 if __name__ == "__main__" :
